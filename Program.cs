d29fb959b08380fbc05316e564ed07b960191ed0
arianhit <93319255+arianhit@users.noreply.github.com>
2022-02-25 17:34:39 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,45 +11,31 @@ namespace Performance
     {
         static void Main(string[] args)
         {
+            //we need an sql connection variable
             SqlConnection con;
+            //we need the database address as an string
             string str;
-            //string courseName;
-            //int courseTotalHours;
-            //int courseId;
+
+
             try
             {
-                str= @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename= G:\My Drive\University Sheffieeld Hallam\Y1\Software Project\Performance\Performance\ERDTabales.mdf ;Integrated Security=True";
+                //database address
+                str = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename= G:\My Drive\University Sheffieeld Hallam\Y1\Software Project\Performance\Performance\ERDTabales.mdf ;Integrated Security=True";
+
+                //creating connection with the database address
                 con = new SqlConnection(str);
+
+                //open connection
                 con.Open();
+                //writing a massage that databas is connected
                 Console.WriteLine("Database Connected");
 
-                //Console.WriteLine("\n Enter Your Course id");
-                //courseId = Convert.ToInt32(Console.ReadLine());
-                //Console.WriteLine("\n Enter Your Course name");
-                //courseName = Console.ReadLine();
-                //Console.WriteLine("\n Enter Your Course total hours");
-                //courseTotalHours = Convert.ToInt32(Console.ReadLine());
-                //string queryInsert = "INSERT INTO Course (Course_Id,Course_Name,Course_Total_Hours) VALUES (" + courseId + ",'" + courseName + "'," + courseTotalHours + ") ";
-
-                //SqlCommand ins = new SqlCommand(queryInsert, con);
-                //ins.ExecuteNonQuery();
-
-                //Console.WriteLine("\n Data Stored Into Database");
-
-                //string queryRead = "SELECT * FROM Course";
-                //SqlCommand read = new SqlCommand(queryRead, con);
-                //SqlDataReader dtreader = read.ExecuteReader();
-                //while(dtreader.Read())
-                //{
-                //    Console.WriteLine("\n Course name :" + dtreader.GetValue(1).ToString());
-                //    Console.WriteLine("\n Course Total hours :" + dtreader.GetValue(2).ToString());
-                //}
-            } 
+            }
             catch (SqlException x)
             {
                 Console.WriteLine(x.Message);
             }
-            Console.ReadLine();
+            
         }
     }
 }
